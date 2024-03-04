45e489ff74bb3866bbdfede3a31d19164dbabc59
Amps <amps@kth.se>
2024-03-04 13:39:36 +0100

    new update

--- Data_visualization.py
+++ Data_visualization.py
@@ -35,13 +35,10 @@ for i in range (1,8):
     a = ith_matrix[len(ith_matrix)-1,0]
     
         
-# Print the data matrix
-#print(data_matrix)
 data_matrix_shape =data_matrix.shape
-#print(data_matrix_shape)
 
-xdata = data_matrix[:,0]/3600
-ydata = data_matrix[:,1]
+xdata = data_matrix[:,0]/3600 #time
+ydata = data_matrix[:,1] #voltage
 
 plt.plot(xdata, ydata, label="Voltage-Time curve")
 plt.xlabel('Time(hr)')
@@ -61,15 +58,26 @@ V = 0.0000125  # Volume in m^3
 R = 0.08134    # Gas constant in bar * m^3 / (mol * K)
 T = 308        # Temperature in Kelvin
 Pressure = 1.214 
+#Converting it into number of moles
 Totalfraction = gases.sum(axis = 1)
 Molefractions = gases.div(Totalfraction, axis= 0)
 
-Partial_pressure = np.multiply(Pressure,Molefractions)/1000
+Partial_pressure = np.multiply(Pressure,Molefractions)
 number_of_moles = Partial_pressure * V / (R * T)
 
 
-plt.plot(Time, number_of_moles)
-plt.xlabel('Time(hr)')
-plt.ylabel('Gases(no. of moles)')
-plt.title(' Gas evolution Curve')
-plt.savefig('Gasevolution_curve.png')
+#plotting these datas together
+fig, ax = plt.subplots(2)
+ax[0].plot(xdata, ydata, color='blue', alpha=0.5)
+ax[0].set_xlabel('Time (hr)')
+ax[0].set_ylabel('Voltage (V)', color = 'grey')
+ax[0].set_xlim(0,61)
+#ax[1].plot(Time/3600, number_of_moles[['Hydrogen.1','Ethylene.1','Carbon dioxide.1']], alpha=0.7)
+ax[1].plot(Time, number_of_moles['Hydrogen'], label='Hydrogen', color='#ff7fb9', alpha=0.7)
+ax[1].plot(Time, number_of_moles['Ethylene'], label='Ethylene', color='purple', alpha=0.6)
+ax[1].plot(Time, number_of_moles['Carbon dioxide'], label='CO2', color='#17becf', alpha=0.7)
+ax[1].set_ylabel('No. of moles', color = 'grey')
+ax[1].set_xlabel('Time (hr)', color = 'grey')
+ax[1].set_xlim(0,61)
+ax[1].legend(['Hydrogen m/z = 1','Ethylene m/z = 27','Carbon dioxide m/z = 44'], loc = 'upper left', fontsize='xx-small', edgecolor= 'None')
+plt.savefig('Plating.png', dpi=300)
